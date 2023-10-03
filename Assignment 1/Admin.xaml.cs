d0f7dc391bd37819e016a570ae7bb3b553fea718
Mary <124908298+2395038@users.noreply.github.com>
2023-10-03 19:36:00 -0400

    Assignment 1 first draft

---
+++ Assignment 1/Admin.xaml.cs
@@ -0,0 +1,265 @@
+﻿using Npgsql;
+using System;
+using System.Buffers.Text;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Npgsql.Replication.PgOutput.Messages.RelationMessage;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+
+namespace Assignment_1
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        public static NpgsqlConnection con; 
+
+        public static NpgsqlCommand cmd; 
+        private void insert_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                establishConnect();
+               
+                con.Open();
+                
+                string Query = "insert into products values(@name, @id, @amount, @price, default";
+                
+                cmd = new NpgsqlCommand(Query, con); 
+
+                cmd.Parameters.AddWithValue("@name", name.Text);
+                cmd.Parameters.AddWithValue("@id", int.Parse(id.Text));
+                cmd.Parameters.AddWithValue("@amount", double.Parse(amount.Text));
+                cmd.Parameters.AddWithValue("@price", double.Parse(price.Text));
+
+                cmd.ExecuteNonQuery();
+                
+                MessageBox.Show("Product Created Successfully");
+                
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void establishConnect()
+        {
+            try
+            {
+                con = new NpgsqlConnection(get_ConnectionString());
+                MessageBox.Show("Connection Established");
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private string get_ConnectionString()
+        {
+            string host = "Host=localhost;";
+            string port = "Port=5432;";
+            string dbName = "Database=VanierFall2023;";
+            string userName = "Username=postgres;";
+            string password = "Password=;";
+
+            string connectionString = string.Format("{0}{1}{2}{3}{4}", host, port, dbName, userName, password);
+            return connectionString;
+
+
+        }
+
+        private void show_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                establishConnect();
+                
+                con.Open();
+                
+                string Query = "select * from students";
+                
+                cmd = new NpgsqlCommand(Query, con);
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+ 
+                dataGrid.ItemsSource = dt.AsDataView(); 
+                DataContext = da;
+                
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                establishConnect();
+                
+                con.Open();
+                
+                string Query = "select * from products where id=@id;";
+               
+                cmd = new NpgsqlCommand(Query, con);
+                
+                cmd.Parameters.AddWithValue("@id", int.Parse(search.Text));
+                
+                bool noData = true;
+                
+                NpgsqlDataReader dr = cmd.ExecuteReader(); 
+                
+                while (dr.Read())
+                {
+                    noData = false;
+                    name.Text = dr["product_name"].ToString();
+                    id.Text = dr["product_id"].ToString();
+                    amount.Text = dr["product_amount"].ToString();
+                    price.Text = dr["product_price"].ToString();
+                    //date.Text = dr["enter_date"].ToString();
+                }
+                if (noData) // checking the data Retrival
+                {
+                    MessageBox.Show("No product with that id");
+                }
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                establishConnect(); 
+
+                con.Open();
+
+                string Query = "UPDATE products SET name = @name, amount = @amount, price = @price WHERE id = @id";
+
+                cmd = new NpgsqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@name", name.Text);
+                cmd.Parameters.AddWithValue("@id", int.Parse(id.Text));
+                cmd.Parameters.AddWithValue("@amount", double.Parse(amount.Text));
+                cmd.Parameters.AddWithValue("@price", double.Parse(price.Text));
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Product Updated Successfully");
+
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                establishConnect(); 
+
+                con.Open();
+
+                string Query = "DELETE FROM products WHERE id = @id";
+
+                cmd = new NpgsqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@id", int.Parse(id.Text));
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Product Deleted Successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Product with ID " + int.Parse(id.Text) + " not found.");
+                }
+
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        
+            }
+
+        private void search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                establishConnect(); 
+
+                con.Open();
+
+                string Query = "SELECT * FROM products WHERE id = @id";
+
+                cmd = new NpgsqlCommand(Query, con);
+
+                cmd.Parameters.AddWithValue("@id", int.Parse(id.Text));
+
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                   
+                    string productName = reader["name"].ToString();
+                    double productAmount = Convert.ToDouble(reader["amount"]);
+                    double productPrice = Convert.ToDouble(reader["price"]);
+
+                    MessageBox.Show("Product Found:" +
+                                    "\nName: " + productName +
+                                    "\nAmount: " + productAmount +
+                                    "\nPrice: " + productPrice);
+                }
+                else
+                {
+                    MessageBox.Show("Product with ID " + int.Parse(id.Text) + " not found.");
+                }
+
+                con.Close();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Assignment 1/Sales.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Assignment_1
+{
+    /// <summary>
+    /// Interaction logic for Sales.xaml
+    /// </summary>
+    public partial class Sales : Window
+    {
+        public Sales()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Total_Click(object sender, RoutedEventArgs e)
+        {
+            double appleAmount = 0;
+            double applePrice = 0.0;
+
+            double orangeAmount = 0;
+            double orangePrice = 0.0;
+
+            double raspberryAmount = 0;
+            double raspberryPrice = 0.0;
+
+            double blueberryAmount = 0;
+            double blueberryPrice = 0.0;
+
+            double cauliflowerAmount = 0;
+            double cauliflowerPrice = 0.0;
+
+            double otherAmount = 0;
+            double otherPrice = 0.0;
+
+            
+            if (!string.IsNullOrEmpty(Amount1.Text))
+            {
+                appleAmount = double.Parse(Amount1.Text);
+                applePrice = double.Parse(Price1.Text);
+            }
+
+            if (!string.IsNullOrEmpty(Amount2.Text))
+            {
+                orangeAmount = double.Parse(Amount2.Text);
+                orangePrice = double.Parse(Price2.Text);
+            }
+
+            if (!string.IsNullOrEmpty(Amount3.Text))
+            {
+                raspberryAmount = double.Parse(Amount3.Text);
+                raspberryPrice = double.Parse(Price3.Text);
+            }
+
+            if (!string.IsNullOrEmpty(Amount4.Text))
+            {
+                blueberryAmount = double.Parse(Amount4.Text);
+                blueberryPrice = double.Parse(Price4.Text);
+            }
+
+            if (!string.IsNullOrEmpty(Amount5.Text))
+            {
+                cauliflowerAmount = double.Parse(Amount5.Text);
+                cauliflowerPrice = double.Parse(Price5.Text);
+            }
+
+            if (!string.IsNullOrEmpty(Amount6.Text))
+            {
+                otherAmount = double.Parse(Amount6.Text);
+                otherPrice = double.Parse(Price6.Text);
+            }
+
+            
+            double total = appleAmount * applePrice + orangeAmount * orangePrice +
+                           raspberryAmount * raspberryPrice + blueberryAmount * blueberryPrice +
+                           cauliflowerAmount * cauliflowerPrice + otherAmount * otherPrice;
+
+            MessageBox.Show("Total is " + total);
+        }
+        }
+}
