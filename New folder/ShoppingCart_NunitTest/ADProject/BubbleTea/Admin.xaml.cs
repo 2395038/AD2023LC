0c9656bf61185bbf0b11710e7777dc4876937393
Mary <124908298+2395038@users.noreply.github.com>
2023-11-05 09:14:45 +0800

    testingtemp

---
+++ New folder/ShoppingCart_NunitTest/ADProject/BubbleTea/Admin.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BubbleTea
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void select_Click(object sender, RoutedEventArgs e)
+        {
+            if (radio1.IsChecked == true)
+            {
+                // Create an instance of Form1
+                EmployeeMangment  employeMangment = new EmployeeMangment();
+
+                // Show Form1
+                employeMangment.Show();
+            }
+            else if (radio2.IsChecked == true)
+            {
+                Inventroy   inventroy = new Inventroy();    
+                inventroy.Show();
+            }
+            else if (radio3.IsChecked == true)
+            {
+                Product product = new Product();    
+                product.Show();
+            }
+            else { 
+            // Handle the case when radioButton1 is not selected
+            MessageBox.Show("Please select radioButton1 to open Form1.");
+            }
+        }
+
+     
+    }
+}
---
+++ New folder/ShoppingCart_UnitTest/Models/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart_UnitTest.Models
+{
+    public class ShoppingCart
+    {
+        //method
+        public double getCheckOut(int q, double p)
+        {
+            return q * p;
+        }
+
+    }
+}
